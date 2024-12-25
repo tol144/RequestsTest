461e55a7d3184c0c90b5f7d84253671f5166c49e
tol <toltol1992toltol@gail.com>
2024-12-25 21:39:25 +0300

    new content types

--- routers/api.py
+++ routers/api.py
@@ -1,11 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 from database.orm.methods import insert_request_result
+from typing import List
 
 
 api_router = APIRouter()
 
 
+@api_router.post("/save_json")
+async def create_new_order(data: dict):
+    await insert_request_result(str(data))
+
+
 @api_router.post("/save_data")
-async def create_new_order(**kwargs):
-    await insert_request_result(str(kwargs))
+async def create_new_order(request: Request):
+    request_data = await request.form()
+    data = {key: value for key, value in request_data.items()}
+    await insert_request_result(str(data))
